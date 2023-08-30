7a4ff75d2c80d2259e3a7668b5edd74ea250ee6d
Rylie Castell <rylie.castell@gmail.com>
2023-08-30 15:26:38 +0200

    print out car information

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,8 @@ public class Main {
         books.setBook(1, "Kass Morgan", "The 100", "217621-127618");
         books.setBook(2, "Suzanne Collins", "The Hunger Games", "279-237");
         System.out.println(books);
+        Car myCar = new Car("Volkswagen", "Bulli 3", 1967, 5);
+        System.out.println(myCar);
+
     }
 }
