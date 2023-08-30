00981c44fff229ef5ff12ede78ed530162fb430b
Rylie Castell <rylie.castell@gmail.com>
2023-08-30 15:20:23 +0200

    finish excercise

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,3 +1,24 @@
+import java.util.Arrays;
+
 public class Library {
-    String[] books;
+    Book[] books = new Book[3];
+
+    public Library() {
+        books[0] = new Book();
+        books[1] = new Book();
+        books[2] = new Book();
+    }
+
+    public void setBook(int index, String author, String title, String isbn) {
+        books[index].setAuthor(author);
+        books[index].setTitle(title);
+        books[index].setIsbn(isbn);
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "books=" + Arrays.toString(books) +
+                '}';
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        Library books = new Library();
+        books.setBook(0, "Judith Kerr", "Als Hitler das rosa Kaninchen stahl", "2128-2381");
+        books.setBook(1, "Kass Morgan", "The 100", "217621-127618");
+        books.setBook(2, "Suzanne Collins", "The Hunger Games", "279-237");
+        System.out.println(books);
     }
 }
